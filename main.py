4f169f09ce91bb4bfba62e17a83a1717a5698b0f
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-02-19 14:26:17 -0500

    Camera test

--- main.py
+++ main.py
@@ -3,16 +3,60 @@
 import time
 import zmq
 
+import numpy as np
+
 import picamera
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+import picamera.array
+#from picamera.array import PiRGBArray
 
 from Adafruit_MotorHAT import Adafruit_MotorHAT
 
-# create a default object, no changes to I2C address or frequency
+# Create a default object, no changes to I2C address or frequency
 motorhat = Adafruit_MotorHAT(addr=0x60)
 leftMotor = motorhat.getMotor(1)
 rightMotor = motorhat.getMotor(2)
+
+# Create camera object
+camera = picamera.PiCamera()
+
+# Numpy array of shape (rows, columns, colors)
+imgArray = picamera.array.PiRGBArray(camera)
+
+
+
+startTime = time.time()
+for i in range(0, 100):
+    camera.capture(imgArray, 'rgb')
+    print(imgArray.shape)
+endTime = time.time()
+
+frameRate = 100 / (endTime - startTime)
+print('fps: %s' % fps)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #leftMotor.run(Adafruit_MotorHAT.FORWARD)
